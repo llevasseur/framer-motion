f23575b94f57b8d7afa21df84292d4d6e897f7e4
Leevon Levasseur <leevonlevasseur@gmail.com>
2024-06-07 11:12:00 -0700

    add - square transformation page

--- client/src/components/Square/Square.jsx
+++ client/src/components/Square/Square.jsx
@@ -1,7 +1,51 @@
 import "./Square.scss";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+import Input from "../../components/Input/Input";
+
+const fadeIn = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+};
 
 const Square = () => {
-  return <div>Square</div>;
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [rotate, setRotate] = useState(0);
+  return (
+    <motion.div
+      className="square"
+      variants={fadeIn}
+      initial="initial"
+      animate="animate"
+    >
+      <div>
+        <motion.div
+          className="square__box"
+          animate={{ x, y, rotate }}
+          transition={{ type: "spring" }}
+        />
+      </div>
+      <div className="inputs">
+        <Input color={"red"} value={x} set={setX}>
+          x
+        </Input>
+        <Input color={"red"} value={y} set={setY}>
+          y
+        </Input>
+        <Input
+          color={"red"}
+          value={rotate}
+          set={setRotate}
+          min={-180}
+          max={180}
+        >
+          rotate
+        </Input>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Square;
